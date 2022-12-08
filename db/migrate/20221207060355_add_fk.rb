6b5e63f96f1d8920a5a8953f7abca320b3472e0f
K. Jetnipit <jetnipit54@gmail.com>
2022-12-08 15:13:28 +0700

    Containerize ruby webapp

--- db/migrate/20221207060355_add_fk.rb
+++
@@ -1,6 +0,0 @@
-class AddFk < ActiveRecord::Migration[7.0]
-  def change
-    add_references :items, :user 
-    add_foreign_key :items,:users
-  end
-end
--- db/migrate/20221207065739_add_user_to_items.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToItems < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :items, :user 
-    add_foreign_key :items,:users
-  end
-end
