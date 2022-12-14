18db665ce8d980217fe6e38d9524c5cfedbfbe8f
K. Jetnipit <jetnipit54@gmail.com>
2022-12-14 19:54:48 +0700

    add date filter in top seller

--- webapp/app/controllers/history_controller.rb
+++ webapp/app/controllers/history_controller.rb
@@ -11,10 +11,25 @@ class HistoryController < ApplicationController
   end
 
   def get_top_seller
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+
+    if @start_date && @end_date then
+      if (@start_date > @end_date)
+        redirect_to "/top_seller", notice: "Invalid start and end date"
+      end
+
+      @sellers = Inventory.select("users.email", "users.name", "SUM(inventories.price*inventories.qty) as total_price")
+      .where("inventories.created_at between ? and ?", @start_date, @end_date)
+      .joins("left join users on users.id = inventories.seller_id")
+      .group("users.email", "users.name")
+      .order("total_price DESC")
+      return
+    end
+
     @sellers = Inventory.select("users.email", "users.name", "SUM(inventories.price*inventories.qty) as total_price")
     .joins("left join users on users.id = inventories.seller_id")
     .group("users.email", "users.name")
     .order("total_price DESC")
   end
-
 end
